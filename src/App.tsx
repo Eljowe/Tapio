bef7e0bbc4e53a706d65ad94f022cdebb25c021e
Eljowe <joelwickstroem@gmail.com>
2023-07-11 09:03:48 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className='text-white w-[100vw] flex flex-col justify-center items-center p-6'>
+      <h1 className='text-9xl tracking-widest'>TOTE/TAPIO</h1>
+      <img className="w-max h-[10em] mt-10" src="https://www.toukolanteras.fi/wp-content/themes/wp-toukolan/images/logo.png"></img>
+    </div>
+  )
+}
+
+export default App
