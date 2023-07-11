fc9ef5653a61a736864f8bc3ac7f0d5388cd0716
Eljowe <joelwickstroem@gmail.com>
2023-07-11 09:26:48 +0300

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Team1 from './assets/Team1.jpg'
+
 function App() {
 
   return (
-    <div className='text-white w-[100vw] flex flex-col justify-center items-center p-6'>
-      <h1 className='text-9xl tracking-widest'>TOTE/TAPIO</h1>
-      <img className="w-max h-[10em] mt-10" src="https://www.toukolanteras.fi/wp-content/themes/wp-toukolan/images/logo.png"></img>
+    <div className='text-white w-full flex flex-col justify-center items-center'>
+      <h3 className='text-2xl tracking-[2vw] sm:tracking-[1em] md:tracking-[1.5em]'>TOUKOLAN TERÄS</h3>
+      <h1 className='text-5xl sm:text-8xl md:text-8xl tracking-widest p-6'>TOTE/TAPIO</h1>
+      <img className="w-max h-[10vh]" src="https://www.toukolanteras.fi/wp-content/themes/wp-toukolan/images/logo.png"></img>
+      <img src={Team1} className='w-[80vw] sm:w-[80vw] md:w-[70vw] lg:w-[60vw] xl:w-[60vw] 2xl:w-[50vw] mt-10 rounded-2xl' alt='Team'></img>
     </div>
   )
 }
